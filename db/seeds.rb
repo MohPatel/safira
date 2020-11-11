84af804e23eda7c169ccb277d791179ab773a8f0
MohPatel <mohiniprincess96@gmail.com>
2020-11-11 10:27:18 -0600

    Added libraries for authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NUMBER_OF_CATEGORY = 5
+PRODUCTS_PER_CATEGORY=5
+
+
+NUMBER_OF_CATEGORY.times do
+  category = Category.create(name: Faker::Name.unique.name)
+  PRODUCTS_PER_CATEGORY.times do
+    product  = category.products.create(
+      name: Faker::Commerce.product_name,
+      price: Faker::Commerce.price,
+      description: Faker::Lorem.sentence(word_count: 3),
+      sale: rand(1..3),
+      priceonsale: Faker::Commerce.price
+    )
+  end
+
+end
+puts "created #{Category.count} Categories"
+puts "Created #{Product.count} Products"AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
