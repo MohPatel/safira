63cdd06bb6158dd1adb6e595d75cf25ca64cd435
MohPatel <mohiniprincess96@gmail.com>
2020-11-11 13:32:35 -0600

    Home controller created

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Product do
  permit_params :name, :price, :description, :sale, :priceonsale, :category_id
 
+ # Domain spefic language
+ # Formstatic gem
+ form do |f|
+  f.semantic_errors # shows errors on :base
+  f.inputs          # builds an input field for every attribute
+  f.inputs do
+    f.input :image, as: :file
+  end
+  f.actions         # adds the 'Submit' and 'Cancel' buttons
+end
 end
