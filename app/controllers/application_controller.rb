d1f0bb783ec81d2445c3caa39b5b1d17450a822a
MohPatel <mohiniprincess96@gmail.com>
2020-11-24 14:33:29 -0600

    Before starting checkout process and updating quantity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,10 @@
 class ApplicationController < ActionController::Base
 before_action :initialize_session
-before_action :increment_visit_count
-helper_method :visit_count
 helper_method :cart
   private
   def initialize_session
-    session[:visit_count] ||= 0
+    session[:quantity] ||= 0
     session[:shopping_cart] ||= []
-  end
-
-  def increment_visit_count
-    session[:visit_count] += 1
-  end
-
-  def visit_count
-    @visit_count = session[:visit_count]
   end
 
   def cart
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,8 +2,11 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to the cart")
     id = params[:id].to_i
+    quantity = params[:quantity]
+    logger.debug("Quantity #{params[:quantity]} to the cart")
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id
+      session[:quantity] = quantity
       product = Product.find(id)
       flash[:notice] = "#{product.name} are added into cart"
     end
@@ -13,6 +16,7 @@ class CartController < ApplicationController
   def destroy
     id = params[:id].to_i
     session[:shopping_cart].delete(id)
+    session[:quantity] = 0
     product = Product.find(id)
     flash[:notice] = "#{product.name} are removed into cart"
     redirect_to root_path
