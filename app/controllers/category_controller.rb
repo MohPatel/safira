65f8f9bacbff32a283b3297aa32240d12fa050d1
MohPatel <mohiniprincess96@gmail.com>
2020-12-09 12:22:43 -0600

    Make Different method for implementing search functionality

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -6,9 +6,7 @@ class CategoryController < ApplicationController
     @products = Product.includes(:category).all.page(params[:page])
   end
   def search
-    # wildcard_search = "%#{params[:keywords]}%"
-    # @products = Product.where("name Like ?", wildcard_search)
-    @products ||= find_products
+   @products ||= find_products
   end
   private
   def find_products
